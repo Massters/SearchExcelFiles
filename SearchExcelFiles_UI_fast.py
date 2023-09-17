8cb780a46a9c848d76f9cade9ab2c451b4ea4082
Ralts <1094722915@qq.com>
2023-09-17 11:43:55 +0800

    feat: 完善注释

--- SearchExcelFiles_UI_fast.py
+++ SearchExcelFiles_UI_fast.py
@@ -6,12 +6,14 @@ from openpyxl.utils import get_column_letter
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QInputDialog, QVBoxLayout, QWidget, QLabel, QTableWidget, QTableWidgetItem, QHBoxLayout, QAction, QCheckBox, QMessageBox, QDialog, QCheckBox, QPushButton, QLineEdit
 from PyQt5.QtCore import Qt, QObject, QRunnable, QThreadPool, pyqtSignal
 
+# 定义了自定义的信号用于在Excel搜索任务中发射信号
 class ExcelSearchTaskSignals(QObject):
     foundKeyword = pyqtSignal(str, str, int, int, str)
     foundAdditionalContent = pyqtSignal(str)
     finished = pyqtSignal()
     error = pyqtSignal(str)
 
+# 表示一个Excel搜索任务.在后台线程中执行实际的Excel搜索操作,并通过信号将搜索结果传递至主窗口
 class ExcelSearchTask(QRunnable):
     def __init__(self, file_path, keyword, use_regex):
         super().__init__()
@@ -54,11 +56,12 @@ class ExcelSearchTask(QRunnable):
 
         self.signals.finished.emit()
 
+# 表示应用程序的主窗口,它包含了用户界面的布局, 控件和与搜索任务相关的处理方法
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-
+    # 定义了程序的用户界面布局,
     def initUI(self):
         mainWidget = QWidget()
         mainLayout = QVBoxLayout()
@@ -66,6 +69,7 @@ class MainWindow(QMainWindow):
         # titleLabel = QLabel("Excel Search")
         # titleLabel.setStyleSheet("font-size: 20px; font-weight: bold; margin-bottom: 10px;")
 
+        # 定义表格用于显示搜索结果
         self.tableWidget = QTableWidget()
         self.tableWidget.setColumnCount(5)
         self.tableWidget.setHorizontalHeaderLabels(["File", "Sheet", "Cell", "Content", "Additional Content"])
@@ -79,6 +83,7 @@ class MainWindow(QMainWindow):
         mainWidget.setLayout(mainLayout)
         self.setCentralWidget(mainWidget)
 
+        # 定义菜单栏用于打开文件夹
         openFolder = QAction('Open Folder', self)
         openFolder.setShortcut('Ctrl+O')
         openFolder.triggered.connect(self.openFolderDialog)
@@ -91,6 +96,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Excel Search')
         self.show()
 
+    # 打开文件夹对话框
     def openFolderDialog(self):
         dialog = QFileDialog()
         dialog.setFileMode(QFileDialog.DirectoryOnly)
@@ -100,6 +106,10 @@ class MainWindow(QMainWindow):
             folder_path = dialog.selectedFiles()[0]
             self.searchExcelFiles(folder_path)
 
+    ################################################################################################################################################################
+    # 遍历选定的文件夹中所有的Excel文件,并为每个文件创建一个ExcelSearchTask实例, 然后将其添加到线程池中,通过连接信号和槽函数, 搜索任务的结果将传递给handleKeywordFound和
+    # handleAdditionalContentFound方法,这些方法负责将结果显示在表格中
+    ################################################################################################################################################################
     def searchExcelFiles(self, folder_path):
         dialog = QDialog()
         dialog.setWindowTitle('Enter Keyword')
@@ -128,7 +138,7 @@ class MainWindow(QMainWindow):
 
         for root, dirs, files in os.walk(folder_path):
             for file in files:
-                if file.endswith(".xlsx") or file.endswith(".xls"):
+                if file.endswith(".xlsx") or file.endswith(".xlsm"):
                     file_path = os.path.join(root, file)
                     task = ExcelSearchTask(file_path, keyword, use_regex)
                     task.signals.foundKeyword.connect(self.handleKeywordFound)
@@ -137,6 +147,7 @@ class MainWindow(QMainWindow):
                     task.signals.error.connect(self.handleTaskError)
                     self.threadpool.start(task)
 
+    # 用于处理在Excel文件中找到的关键字事件, 它将搜索结果添加到表格中的相应单元格
     def handleKeywordFound(self, file_path, sheet_name, col_idx, row_idx, cell_value):
         row_count = self.tableWidget.rowCount()
         self.tableWidget.insertRow(row_count)
@@ -146,13 +157,16 @@ class MainWindow(QMainWindow):
         self.tableWidget.setItem(row_count, 2, QTableWidgetItem(f"{get_column_letter(col_idx)}{row_idx}"))
         self.tableWidget.setItem(row_count, 3, QTableWidgetItem(cell_value))
 
+    # 用于处理在Excel文件中找到的附加内容事件, 它将附加内容添加到表格中的相应单元格
     def handleAdditionalContentFound(self, additional_content):
         row_count = self.tableWidget.rowCount()
         self.tableWidget.setItem(row_count - 1, 4, QTableWidgetItem(additional_content))
 
+    # 用于处理搜索任务完成事件
     def handleTaskFinished(self):
         pass
 
+    # 用于处理搜索任务发生错误的事件, 它显示一个错误的消息框
     def handleTaskError(self, error_msg):
         QMessageBox.critical(self, "Error", error_msg)
 
